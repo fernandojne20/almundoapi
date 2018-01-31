8ec1689f5833995f510bee02c8608705914a58fd
Fernando Nuñez <fernando.nunez@qrvey.com>
2018-01-31 09:32:26 -0500

    database production

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/al-mundo'
+    // url: 'mongodb://localhost:27017/al-mundo'
+    url: 'mongodb://almundo:almundopass@ds221228.mlab.com:21228/heroku_t9d0vw84'
 }
